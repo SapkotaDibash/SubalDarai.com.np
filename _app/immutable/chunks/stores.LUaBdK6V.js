030a1380e9c82f6e1f03d664e66fad0a8637187e
Dibash Sapkota _ <92871130+SapkotaDibash@users.noreply.github.com>
2024-03-09 10:36:32 +0545

    upload files

---
+++ _app/immutable/chunks/stores.LUaBdK6V.js
@@ -0,0 +1,1 @@
+const c="https://script.google.com/macros/s/AKfycbyKhnE5ehayOis6AS0xXIcPcfqbLROvMuERZFfTHX7tS5JZONfwpy0PyAZCitHOPe2SbA/exec?";export{c as A};
---
+++ _app/immutable/entry/start.BOOkvgXq.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Chv4luPj.js";export{t as start};
